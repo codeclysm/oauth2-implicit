5d5ae2d83a52bbc69db347bc3a06db5c9ce3d144
Matteo Suppo <matteo.suppo@gmail.com>
2016-09-07 15:41:52 +0200

    Ensure nothing escapes the closure

--- oauth2-implicit.js
+++ oauth2-implicit.js
@@ -1,9 +1,9 @@
 ;
 var Oauth2 = (function () {
     function Oauth2(opts) {
-        this.opts = defaults(opts);
+        this.opts = this.defaults(opts);
         // Attempt to get the token from the url
-        var token = getTokenFromHash(window.location, opts.params);
+        var token = this.getTokenFromHash(window.location, opts.params);
         if (token.token !== '') {
             this.token = new Promise(function (resolve, reject) { return resolve(token); });
             return;
@@ -14,98 +14,100 @@ var Oauth2 = (function () {
     Oauth2.prototype.refresh = function () {
         var _this = this;
         // Attempt to get the token from an iframe redirect
-        this.token = redirect(this.opts);
+        this.token = this.redirect(this.opts);
         // Refresh the token
         this.token.then(function (token) {
             window.setTimeout(_this.refresh.bind(_this), token.expires * 1000);
         });
     };
+    Oauth2.prototype.defaults = function (opts) {
+        if (!opts.params) {
+            opts.params = {};
+        }
+        if (!opts.params.token) {
+            opts.params.token = 'access_token';
+        }
+        if (!opts.params.expires) {
+            opts.params.expires = 'expires_in';
+        }
+        if (!opts.params.type) {
+            opts.params.type = 'token_type';
+        }
+        if (!opts.params.scope) {
+            opts.params.scope = 'scope';
+        }
+        if (!opts.authURI) {
+            console.error('Oauth2: the property authURI is missing');
+        }
+        if (!opts.authURI) {
+            console.error('Oauth2: the property authURI is missing');
+        }
+        return opts;
+    };
+    // redirect creates an iframe and redirects the iframe content to the oauth2 endpoint 
+    Oauth2.prototype.redirect = function (opts) {
+        var _this = this;
+        return new Promise(function (resolve, reject) {
+            var iframe = document.createElement('iframe');
+            iframe.setAttribute('src', _this.getRedirect(opts));
+            iframe.setAttribute('height', '0');
+            iframe.setAttribute('width', '0');
+            document.body.appendChild(iframe);
+            var getTokenFromHash = _this.getTokenFromHash;
+            iframe.onload = function () {
+                var token = getTokenFromHash(this.contentWindow.location, opts.params);
+                document.body.removeChild(iframe);
+                if (token.token === '') {
+                    reject('token not found');
+                }
+                else {
+                    resolve(token);
+                }
+            };
+        });
+    };
+    // getRedirect builds a redirect uri to get an implicit flow token
+    Oauth2.prototype.getRedirect = function (opts) {
+        var redirectUri = encodeURIComponent(opts.redirectURI);
+        var state = Math.random().toString(36).substr(2, 8);
+        var scope = opts.scopes.join(',');
+        var uri = opts.authURI + '?client_id=' + opts.clientID + '&state=' + state + '&scope=' + scope + '&response_type=token&redirect_uri=' + redirectUri;
+        return uri;
+    };
+    // getTokenFromHash searches for the given param inside the location hash
+    // the location hash is usually in the form #access_token=XYXYXY&expire=blabla
+    Oauth2.prototype.getTokenFromHash = function (location, params) {
+        // getvalue extract a value from an array of "key=value" strings
+        function getValue(vars, param) {
+            for (var i = 0; i < vars.length; i++) {
+                var _a = vars[i].split('='), name_1 = _a[0], value = _a[1];
+                if (name_1 === param) {
+                    return value;
+                }
+            }
+            return '';
+        }
+        var token = {
+            token: '',
+            scope: '',
+            expires: 0,
+            type: ''
+        };
+        try {
+            var hash_1 = location.hash.substring(1);
+        }
+        catch (e) {
+            console.error('Oauth2: Get the hash from the location: ', e);
+            return token;
+        }
+        var hash = location.hash.substring(1);
+        var vars = hash.split('&');
+        token.token = getValue(vars, params.token);
+        token.expires = parseInt(getValue(vars, params.expires));
+        token.scope = getValue(vars, params.scope);
+        token.type = getValue(vars, params.type);
+        return token;
+    };
     return Oauth2;
 }());
-function defaults(opts) {
-    if (!opts.params) {
-        opts.params = {};
-    }
-    if (!opts.params.token) {
-        opts.params.token = 'access_token';
-    }
-    if (!opts.params.expires) {
-        opts.params.expires = 'expires_in';
-    }
-    if (!opts.params.type) {
-        opts.params.type = 'token_type';
-    }
-    if (!opts.params.scope) {
-        opts.params.scope = 'scope';
-    }
-    if (!opts.authURI) {
-        console.error('Oauth2: the property authURI is missing');
-    }
-    if (!opts.authURI) {
-        console.error('Oauth2: the property authURI is missing');
-    }
-    return opts;
-}
-// redirect creates an iframe and redirects the iframe content to the oauth2 endpoint 
-function redirect(opts) {
-    return new Promise(function (resolve, reject) {
-        var iframe = document.createElement('iframe');
-        iframe.setAttribute('src', getRedirect(opts));
-        iframe.setAttribute('height', '0');
-        iframe.setAttribute('width', '0');
-        document.body.appendChild(iframe);
-        iframe.onload = function () {
-            var token = getTokenFromHash(this.contentWindow.location, opts.paramName);
-            document.body.removeChild(iframe);
-            if (token.token === '') {
-                reject('token not found');
-            }
-            else {
-                resolve(token);
-            }
-        };
-    });
-}
-// getRedirect builds a redirect uri to get an implicit flow token
-function getRedirect(opts) {
-    var redirectUri = encodeURIComponent(opts.redirectURI);
-    var state = Math.random().toString(36).substr(2, 8);
-    var scope = opts.scopes.join(',');
-    var uri = opts.authURI + '?client_id=' + opts.clientID + '&state=' + state + '&scope=' + scope + '&response_type=token&redirect_uri=' + redirectUri;
-    return uri;
-}
-// getTokenFromHash searches for the given param inside the location hash
-// the location hash is usually in the form #access_token=XYXYXY&expire=blabla
-function getTokenFromHash(location, params) {
-    var token = {
-        token: '',
-        scope: '',
-        expires: 0,
-        type: ''
-    };
-    try {
-        var hash_1 = location.hash.substring(1);
-    }
-    catch (e) {
-        console.error('Oauth2: Get the hash from the location: ', e);
-        return token;
-    }
-    var hash = location.hash.substring(1);
-    var vars = hash.split('&');
-    token.token = getValue(vars, params.token);
-    token.expires = parseInt(getValue(vars, params.expires));
-    token.scope = getValue(vars, params.scope);
-    token.type = getValue(vars, params.type);
-    return token;
-}
-// getvalue extract a value from an array of "key=value" strings
-function getValue(vars, param) {
-    for (var i = 0; i < vars.length; i++) {
-        var _a = vars[i].split('='), name_1 = _a[0], value = _a[1];
-        if (name_1 === param) {
-            return value;
-        }
-    }
-    return '';
-}
 //# sourceMappingURL=oauth2-implicit.js.map
--- oauth2-implicit.ts
+++ oauth2-implicit.ts
@@ -25,9 +25,9 @@ class Oauth2 {
   private opts: Options;
 
   constructor(opts: Options) {
-    this.opts = defaults(opts);
+    this.opts = this.defaults(opts);
     // Attempt to get the token from the url
-    let token = getTokenFromHash(window.location, opts.params);
+    let token = this.getTokenFromHash(window.location, opts.params);
     if (token.token !== '') {
       this.token = new Promise((resolve, reject) => resolve(token));
       return;
@@ -38,7 +38,7 @@ class Oauth2 {
 
   refresh() {
     // Attempt to get the token from an iframe redirect
-    this.token = redirect(this.opts);
+    this.token = this.redirect(this.opts);
 
     // Refresh the token
     this.token.then(token => {
@@ -46,43 +46,42 @@ class Oauth2 {
     });
   }
 
-}
+  private defaults(opts: Options): Options {
+    if (!opts.params) {
+      opts.params = {};
+    }
+    if (!opts.params.token) {
+      opts.params.token = 'access_token';
+    }
+    if (!opts.params.expires) {
+      opts.params.expires = 'expires_in';
+    }
+    if (!opts.params.type) {
+      opts.params.type = 'token_type';
+    }
+    if (!opts.params.scope) {
+      opts.params.scope = 'scope';
+    }
+    if (!opts.authURI) {
+      console.error('Oauth2: the property authURI is missing');
+    }
+    if (!opts.authURI) {
+      console.error('Oauth2: the property authURI is missing');
+    }
+    return opts;
+  }
 
-function defaults(opts: Options): Options {
-  if (!opts.params) {
-    opts.params = {};
-  }
-  if (!opts.params.token) {
-    opts.params.token = 'access_token';
-  }
-  if (!opts.params.expires) {
-    opts.params.expires = 'expires_in';
-  }
-  if (!opts.params.type) {
-    opts.params.type = 'token_type';
-  }
-  if (!opts.params.scope) {
-    opts.params.scope = 'scope';
-  }
-  if (!opts.authURI) {
-    console.error('Oauth2: the property authURI is missing');
-  }
-  if (!opts.authURI) {
-    console.error('Oauth2: the property authURI is missing');
-  }
-  return opts;
-}
-
-// redirect creates an iframe and redirects the iframe content to the oauth2 endpoint 
-function redirect(opts): Promise<Token> {
-  return new Promise((resolve, reject) => {
+  // redirect creates an iframe and redirects the iframe content to the oauth2 endpoint 
+  private redirect(opts): Promise<Token> {
+    return new Promise((resolve, reject) => {
       let iframe = document.createElement('iframe');
-      iframe.setAttribute('src', getRedirect(opts));
+      iframe.setAttribute('src', this.getRedirect(opts));
       iframe.setAttribute('height', '0');
       iframe.setAttribute('width', '0');
       document.body.appendChild(iframe);
+      let getTokenFromHash = this.getTokenFromHash;
       iframe.onload = function () {
-        let token = getTokenFromHash(this.contentWindow.location, opts.paramName);
+        let token = getTokenFromHash(this.contentWindow.location, opts.params);
         document.body.removeChild(iframe);
         if (token.token === '') {
           reject('token not found');
@@ -91,49 +90,50 @@ function redirect(opts): Promise<Token> {
         }
       };
     });
-}
+  }
 
-// getRedirect builds a redirect uri to get an implicit flow token
-function getRedirect(opts: Options): string {
-  let redirectUri = encodeURIComponent(opts.redirectURI);
-  let state = Math.random().toString(36).substr(2, 8);
-  let scope = opts.scopes.join(',');
-  let uri = opts.authURI + '?client_id=' + opts.clientID + '&state=' + state + '&scope=' + scope +  '&response_type=token&redirect_uri=' + redirectUri;
-  return uri;
-}
+  // getRedirect builds a redirect uri to get an implicit flow token
+  private getRedirect(opts: Options): string {
+    let redirectUri = encodeURIComponent(opts.redirectURI);
+    let state = Math.random().toString(36).substr(2, 8);
+    let scope = opts.scopes.join(',');
+    let uri = opts.authURI + '?client_id=' + opts.clientID + '&state=' + state + '&scope=' + scope + '&response_type=token&redirect_uri=' + redirectUri;
+    return uri;
+  }
 
-// getTokenFromHash searches for the given param inside the location hash
-// the location hash is usually in the form #access_token=XYXYXY&expire=blabla
-function getTokenFromHash(location: Location, params: Params): Token {
-  let token = {
-    token: '',
-    scope: '',
-    expires: 0,
-    type: ''
-  };
-  try {
+  // getTokenFromHash searches for the given param inside the location hash
+  // the location hash is usually in the form #access_token=XYXYXY&expire=blabla
+  private getTokenFromHash(location: Location, params: Params): Token {
+    // getvalue extract a value from an array of "key=value" strings
+    function getValue(vars: Array<string>, param: string): string {
+      for (let i = 0; i < vars.length; i++) {
+        let [name, value] = vars[i].split('=');
+        if (name === param) {
+          return value;
+        }
+      }
+      return '';
+    }
+
+    let token = {
+      token: '',
+      scope: '',
+      expires: 0,
+      type: ''
+    };
+    try {
+      let hash = location.hash.substring(1);
+    } catch (e) {
+      console.error('Oauth2: Get the hash from the location: ', e);
+      return token;
+    }
     let hash = location.hash.substring(1);
-  } catch (e) {
-    console.error('Oauth2: Get the hash from the location: ', e);
+    let vars = hash.split('&');
+
+    token.token = getValue(vars, params.token);
+    token.expires = parseInt(getValue(vars, params.expires));
+    token.scope = getValue(vars, params.scope);
+    token.type = getValue(vars, params.type);
     return token;
   }
-  let hash = location.hash.substring(1);
-  let vars = hash.split('&');
-
-  token.token = getValue(vars, params.token);
-  token.expires = parseInt(getValue(vars, params.expires));
-  token.scope = getValue(vars, params.scope);
-  token.type = getValue(vars, params.type);
-  return token;
-}
-
-// getvalue extract a value from an array of "key=value" strings
-function getValue(vars: Array<string>, param: string): string {
-  for (let i = 0; i < vars.length; i++) {
-    let [name, value] = vars[i].split('=');
-    if (name === param) {
-      return value;
-    }
-  }
-  return '';
 }
